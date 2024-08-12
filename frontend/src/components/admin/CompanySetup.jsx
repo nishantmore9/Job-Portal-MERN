5e8e0d1e9588f70252d2bf7cbbb93275f3a16747
Nishant More <nishantmore228@gmail.com>
2024-08-12 23:49:33 +0530

    Add company setup form

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { ArrowLeft } from "lucide-react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const CompanySetup = () => {
-  return (
-    <div>CompanySetup</div>
-  )
-}
+  const [input, setInput] = useState({
+    name: "",
+    description: "",
+    website: "",
+    location: "",
+    file: null,
+  });
 
-export default CompanySetup
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    const file = e.target.files?.[0];
+    setInput({ ...input, file });
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-xl mx-auto my-10">
+        <form action="">
+          <div className="flex items-center gap-5 p-8">
+            <Button
+              variant="outline"
+              className="flex items-center gap-2 text-gray-500 font-semibold"
+            >
+              <ArrowLeft />
+              <span>back</span>
+            </Button>
+            <h1 className="font-bold text-xl">Company setup</h1>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <Label>Company Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={input.name}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={input.description}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Website</Label>
+              <Input
+                type="text"
+                name="website"
+                value={input.website}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Location</Label>
+              <Input
+                type="text"
+                name="location"
+                value={input.location}
+                onChange={changeEventHandler}
+              />
+            </div>
+            <div>
+              <Label>Logo</Label>
+              <Input
+                type="file"
+                accept="image/*"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+          <Button className="w-full my-4">
+            submit
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CompanySetup;
